996f2c651870f0d08c2c523ef65832eb1ae6cf19
Muhammad-UK <muhammadkhan3655@gmail.com>
2024-01-16 11:33:21 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Home } from "./Home";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Guest List!</h1>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
